8b72bdb83a2b07939822c1246475234e06dd4a85
Tamnil Saito Junior <tamnil@gmail.com>
2019-12-21 13:17:24 -0300

    temp canvas

--- my-app-web/src/Sensors.js
+++ my-app-web/src/Sensors.js
@@ -1,38 +1,41 @@
 import React from "react";
 
-
 /* eslint-disable */
 
 function initSensor() {
-let   sensor = new AbsoluteOrientationSensor({ frequency: 60 });
-sensor.onreading = () =>{
-console.log('quaternion read',sensor.quaternion)
-model.quaternion.fromArray(sensor.quaternion);
-
-}
+  let sensor = new AbsoluteOrientationSensor({ frequency: 60 });
+  sensor.onreading = () => {
+    console.log("quaternion read", sensor.quaternion);
+    // model.quaternion.fromArray(sensor.quaternion);
+  };
   sensor.onerror = event => {
     if (event.error.name == "NotReadableError") {
-      console.log("Sensor is not available.",event.error.message);
+      console.log("Sensor is not available.", event.error.message);
     }
   };
   sensor.start();
   return sensor;
 }
 
-console.log('here')
+console.log("here");
+var  a=initSensor();
 
-console.log(initSensor())
+console.log(initSensor());
 
 class Sensors extends React.Component {
   constructor() {
     super();
-    this.state = {color: "red"};
+    this.state = { color: "red" };
   }
   render() {
-    return <h2>Hi, I am a sensor!</h2>;
+    return (
+      <div>
+        <div id="gyro"> gyroData </div>
+        <div id="acc"> accData </div>
+
+      </div>
+    );
   }
 }
 
-
-
-export default  Sensors
+export default Sensors;
