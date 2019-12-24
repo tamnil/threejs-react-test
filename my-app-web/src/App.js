550ab60692bbf7c5978ab3fa28229ebd7274c382
Tamnil Saito Junior <tamnil@gmail.com>
2019-12-24 02:59:45 -0300

    redux init

--- my-app-web/src/App.js
+++ my-app-web/src/App.js
@@ -1,10 +1,26 @@
-'use strict'
-
 import React from "react";
 import "./App.css";
 import Sensors from "./Sensors";
 import CanvasBlock from "./CanvasBlock";
 
+import { createStore } from "redux";
+
+function todos(state = [], action) {
+  switch (action.type) {
+    case "ADD_TODO":
+      return state.concat([action.text + "asdfsdf"]);
+    default:
+      return state;
+  }
+}
+
+
+const store = createStore(todos, ["Use Redux"]);
+store.dispatch({
+  type: "ADD_TODO",
+  text: "Read the docs"
+});
+console.log(store.getState());
 function App() {
   return (
     <div className="App">
@@ -15,5 +31,6 @@ function App() {
   );
 }
 
+console.log();
 
 export default App;
--- my-app-web/src/CanvasBlock.js
+++ my-app-web/src/CanvasBlock.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { gimbal } from "./3d/helpers/gimbal";
